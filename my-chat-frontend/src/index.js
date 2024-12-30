45eae28664d0af4ee432e914ffb91ebd64f97865
Christopher Clark <christopher.d.clark@nps.edu>
2024-12-29 21:11:03 -0500

    front end issues

--- my-chat-frontend/src/index.js
+++ my-chat-frontend/src/index.js
@@ -7,6 +7,22 @@ import theme from './theme';
 // Remove reportWebVitals if not used
 // import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === "production") {
+  console.log = (...args) => {
+    const newArgs = ["[PROD LOG]", ...args];
+    window.console.log.apply(this, newArgs);
+  };
+  console.error = (...args) => {
+    const newArgs = ["[PROD ERROR]", ...args];
+    window.console.error.apply(this, newArgs);
+  };
+  console.warn = (...args) => {
+    const newArgs = ["[PROD WARN]", ...args];
+    window.console.warn.apply(this, newArgs);
+  };
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
