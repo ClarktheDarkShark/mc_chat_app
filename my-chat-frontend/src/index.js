96b060c7084ad6eeef02c746e4eda92a740e4c17
Christopher Clark <christopher.d.clark@nps.edu>
2024-12-29 21:27:50 -0500

    front end issues

--- my-chat-frontend/src/index.js
+++ my-chat-frontend/src/index.js
@@ -7,29 +7,6 @@ import theme from './theme';
 // Remove reportWebVitals if not used
 // import reportWebVitals from './reportWebVitals';
 
-if (process.env.NODE_ENV === "production") {
-  console.log = (...args) => {
-    const newArgs = ["[PROD LOG]", ...args];
-    window.console.log.apply(this, newArgs);
-  };
-  console.error = (...args) => {
-    const newArgs = ["[PROD ERROR]", ...args];
-    window.console.error.apply(this, newArgs);
-  };
-  console.warn = (...args) => {
-    const newArgs = ["[PROD WARN]", ...args];
-    window.console.warn.apply(this, newArgs);
-  };
-}
-
-window.addEventListener("error", function (e) {
-  console.error("Global Error:", e.message, e);
-});
-
-window.addEventListener("unhandledrejection", function (e) {
-  console.error("Unhandled Promise Rejection:", e.reason);
-});
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
