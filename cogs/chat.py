545621fbed06a15b7fd1ef606aceb50c80cdbcb6
Christopher Clark <christopher.d.clark@nps.edu>
2024-12-30 23:43:38 -0500

    file logic

--- cogs/chat.py
+++ cogs/chat.py
@@ -108,7 +108,8 @@ class ChatBlueprint:
 
                 if file:
                     # **SECURELY SAVE THE FILE**
-                    filename = secure_filename(file.filename)
+                    # filename = secure_filename(file.filename)
+                    filename = file.filename
                     unique_filename = f"{uuid.uuid4()}_{filename}"
                     file_path = os.path.join(self.upload_folder, unique_filename)
                     file.save(file_path)
