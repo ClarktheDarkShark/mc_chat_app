b03a26a46d0439b634fc32622d654aa176d8f5fa
Christopher Clark <christopher.d.clark@nps.edu>
2024-12-31 17:06:58 -0500

    file logic

--- app.py
+++ app.py
@@ -73,13 +73,8 @@ def create_app():
     return app
 
 # Create the app instance globally for Gunicorn
-app_instance = FlaskApp()
-app = app_instance.app  # Expose the app instance for Gunicorn
+app = create_app()
 
 if __name__ == "__main__":
-    app_instance.run()
-
-# if __name__ == "__main__":
-#     app = create_app()
-#     port = int(os.getenv("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
